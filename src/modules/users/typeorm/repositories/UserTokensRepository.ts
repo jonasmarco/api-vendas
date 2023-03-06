911a452b499e56bba221e28d02b7c5b088ba5206
Jonas Tolentino <jonasmarco.ti@gmail.com>
2023-03-06 12:17:18 -0300

    feat: UserToken

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,23 @@
+import { EntityRepository, Repository } from 'typeorm'
+import UserToken from '../entities/UserToken'
+
+@EntityRepository(UserToken)
+export default class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    return this.findOne({
+      where: {
+        token
+      }
+    })
+  }
+
+  public async generateToken(user_id: string): Promise<UserToken | undefined> {
+    const userToken = this.create({
+      user_id
+    })
+
+    await this.save(userToken)
+
+    return userToken
+  }
+}
